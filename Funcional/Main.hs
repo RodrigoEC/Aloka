5afb7708b44a0e764b2c142964402fc97c5f7ac2
dnlgomesl <50140741+dnlgomesl@users.noreply.github.com>
2021-04-23 10:52:21 -0300

    fazer locação feito

--- Funcional/Main.hs
+++ Funcional/Main.hs
@@ -51,9 +51,15 @@ telaLogin = do
     cpfUsuario <- lerEntradaString
     verificaUserLogin cpfUsuario
 
+--- verificaUsuarioBD cpfUsuario (verifica se o cpf digitado existe no bd e retorna um bolean)    
 verificaUserLogin::String -> IO()
-verificaUserLogin cpfUsuario = if(not(verificaBD cpfUsuario)) then putStrLn("Usuário não cadastrado")
+verificaUserLogin cpfUsuario = if(not(verificaUsuarioBD cpfUsuario)) then voltarTelaErroLogin
                                                 else(telaLogado cpfUsuario)
+
+voltarTelaErroLogin::IO()
+voltarTelaErroLogin = do
+    putStrLn("Usuário não cadastrado")
+    telaLogin
 
 telaLogado::String -> IO()
 telaLogado cpfUsuario = do
@@ -67,23 +73,53 @@ telaLogado cpfUsuario = do
     putStrLn("[4] Devolução da locação")
     putStrLn("[5] Logout")
     opcao <- lerEntradaInt
-    mudaTelaLogado opcao
+    mudaTelaLogado opcao cpfUsuario
 
-mudaTelaLogado::Int-> IO()
-mudaTelaLogado opcao -- | opcao == 1 = telaListarFilmes (Gustavo)
-                        | opcao == 2 = telaFazerLocacao
-                        | opcao == 3 = telaRecomendacao
-                     -- | opcao == 4 = telaDevolucao (Gustavo)
+mudaTelaLogado::Int-> String-> IO()
+mudaTelaLogado opcao cpfUsuario -- | opcao == 1 = telaListarFilmes cpfUsuario (Gustavo)
+                        | opcao == 2 = telaFazerLocacao cpfUsuario
+                        | opcao == 3 = telaRecomendacao cpfUsuario
+                     -- | opcao == 4 = telaDevolucao cpfUsuario (Gustavo)
                         | otherwise = telaPrincipal
 
 --------------------------------- Sessão Fazer Locação -------------------------------------------------
-telaFazerLocacao::IO()
-telaFazerLocacao = do
-    putStrLn("Tela Locação")
+telaFazerLocacao::String-> IO()
+telaFazerLocacao cpfUsuario = do
+    putStrLn("OBS: Caso queira verificar a lista de filmes digite 'L' no lugar do id do folme.")
+    putStrLn("ID do filme desejado: ")
+    idFilme <- lerEntradaString
+    verificaFilme idFilme cpfUsuario
+
+---- verificaExistenciaFilmeBD: verifica se o filme ta cadastrado no bd - retorna um bolean
+---- verificaDisponibilidadeBD: verifica se ainda tem copia do filme para alugar, no bd -  retorna um bolean
+
+verificaFilme::String-> String-> IO()
+verificaFilme idFilme cpfUsuario|idFilme == "L" = telaListarFilmes cpfUsuario
+                                |not(verificaExistenciaFilmeBD idFilme) = voltarTelaErroFilme cpfUsuario
+                                |not(verificaDisponibilidadeBD idFilme) = voltarTelaErroIndisponivel cpfUsuario
+                                |otherwise = locarFilme idFilme cpfUsuario
+
+voltarTelaErroFilme::String ->IO()
+voltarTelaErroFilme cpfUsuario = do
+    putStrLn("Filme não cadastrado")
+    telaFazerLocacao cpfUsuario
+
+voltarTelaErroIndisponivel::String ->IO()
+voltarTelaErroIndisponivel cpfUsuario = do
+    putStrLn("Filme indisponível")
+    telaFazerLocacao cpfUsuario
+
+---- locarFilmeBD: adiciona um filme no usuario, no bd - retorna uma string "Filme #nomeFilme alugado com sucesso!"
+locarFilme:: String-> String-> IO()
+locarFilme idFilme cpfUsuario = do
+    let alugado = locarFilmeBD
+    putStrLn(alugado)
+    putStrLn("----")
+    telaLogado cpfUsuario
 
 --------------------------------- Sessão Recomendação da Locadora ---------------------------------------
-telaRecomendacao::IO()
-telaRecomendacao = do
+telaRecomendacao::String-> IO()
+telaRecomendacao cpfUsuario = do
     putStrLn("Tela Recomendação")
 
 main::IO()

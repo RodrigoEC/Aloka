f6920376c698f670c4e83c5c221049d611de5367
dnlgomesl <50140741+dnlgomesl@users.noreply.github.com>
2021-04-23 08:43:26 -0300

    métodos de ler entradas

---
+++ Funcional/Main.hs
@@ -0,0 +1,11 @@
+---  Ler entradas
+lerEntradaInt :: IO Int
+lerEntradaInt = do
+         x <- readLn
+         return x
+
+lerEntradaString :: IO String
+lerEntradaString = do
+         x <- getLine
+         return x
+--- 

46ba0ad7cb680ab5c197b319543844eaaf023971
GusttaFS <gustavo.silva@ccc.ufcg.edu.br>
2021-04-23 10:47:30 -0300

    Organizando e adicionando logout na tela principal.

--- Funcional/Main.hs
+++ Funcional/Main.hs
@@ -2,9 +2,8 @@ import System.IO
 import System.IO.Unsafe
 import System.IO.Error 
 import System.Process
-import System.IO.Unsafe(unsafeDupablePerformIO)
 
------------------------------- Ler entradas ---------------------------------------------------------------
+---------- Leitura de entradas ----------
 lerEntradaInt :: IO Int
 lerEntradaInt = do
          x <- readLn
@@ -15,7 +14,7 @@ lerEntradaString = do
          x <- getLine
          return x
 
---- Tela Principal
+------------- Tela Principal -------------
 
 logo :: String
 logo = unsafeDupablePerformIO (readFile "logo.txt")
@@ -31,61 +30,74 @@ telaPrincipal = do
     putStrLn("[1] Login como cliente")
     putStrLn("[2] Login como administrador")
     putStrLn("[3] Cadastro de Usuário")
+    putStrLn("[4] Logout")
 
     opcao <- lerEntradaInt
     mudaTelaInicial opcao
 
---- Muda tela
-mudaTelaInicial::Int-> IO()
-mudaTelaInicial opcao | opcao == 1 = telaLogin
+--------------- Muda tela -----------------
+mudaTelaInicial :: Int -> IO()
+mudaTelaInicial opcao | opcao == 1 = telaLoginCliente
 ---                   | opcao == 2 = telaAdmistrador (Cláudio)
----                   | opcao == 3 = telaCadastroUsuario (Gustavo)
+                      | opcao == 3 = telaCadastroUsuario
+                      | opcao == 4 = putStrLn("end")
                       | otherwise = putStrLn("Erro! Opção inválida")
 
------------------------------------ Sessão de Login ----------------------------------------------
-telaLogin::IO()
-telaLogin = do
+--------------------------------------------
+main :: IO()
+main = do
+    telaPrincipal
+--------------------------------------------
+
+
+
+-------------- Sessão de Login -------------
+telaLoginCliente :: IO()
+telaLoginCliente = do
     system "clear"
     putStrLn("----")
     putStrLn("Usuário:")
+
     cpfUsuario <- lerEntradaString
-    verificaUserLogin cpfUsuario
+    putStrLn ""
+   -- verificaUserLogin cpfUsuario
 
-verificaUserLogin::String -> IO()
-verificaUserLogin cpfUsuario = if(not(verificaBD cpfUsuario)) then putStrLn("Usuário não cadastrado")
-                                                else(telaLogado cpfUsuario)
+--verificaUserLogin :: String -> IO()
+--verificaUserLogin cpfUsuario = if (not(verificaBD cpfUsuario)) 
+                             --       then putStrLn("Usuário não cadastrado")
+                               -- else (telaLogado cpfUsuario)
 
-telaLogado::String -> IO()
+telaLogado :: String -> IO()
 telaLogado cpfUsuario = do
     system "clear"
     putStrLn("----")
-    putStrLn("Bem-vindx ao Aloka, "++ cpfUsuario) --- aqui, ou a gente deixa o bem vindo com o cpf (identificador) ou entao faz uma consulta que retorna o nome desse id
+    putStrLn("Bem-vindx ao Aloka, " ++ cpfUsuario) 
+    --- aqui, ou a gente deixa o bem vindo com o cpf (identificador) ou entao faz uma consulta que retorna o nome desse id
     putStrLn("Como deseja prosseguir?")
     putStrLn("[1] Listar Filmes")
     putStrLn("[2] Fazer locação")
     putStrLn("[3] Recomendação da locadora")
     putStrLn("[4] Devolução da locação")
     putStrLn("[5] Logout")
+
     opcao <- lerEntradaInt
     mudaTelaLogado opcao
 
-mudaTelaLogado::Int-> IO()
-mudaTelaLogado opcao -- | opcao == 1 = telaListarFilmes (Gustavo)
+mudaTelaLogado :: Int -> IO()
+mudaTelaLogado opcao    | opcao == 1 = telaListarFilmes
                         | opcao == 2 = telaFazerLocacao
                         | opcao == 3 = telaRecomendacao
-                     -- | opcao == 4 = telaDevolucao (Gustavo)
+                        | opcao == 4 = telaDevolucao "<<usuario>>"
                         | otherwise = telaPrincipal
 
---------------------------------- Sessão Fazer Locação -------------------------------------------------
-telaFazerLocacao::IO()
+
+-------------- Sessão Fazer Locação -------------
+telaFazerLocacao :: IO()
 telaFazerLocacao = do
     putStrLn("Tela Locação")
 
---------------------------------- Sessão Recomendação da Locadora ---------------------------------------
-telaRecomendacao::IO()
+
+-------- Sessão Recomendação da Locadora ---------
+telaRecomendacao :: IO()
 telaRecomendacao = do
     putStrLn("Tela Recomendação")
-
-main::IO()
-main = do
-    telaPrincipal

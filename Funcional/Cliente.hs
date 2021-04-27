4128b7468d65763f55396c48cd677d99e0fff631
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-04-27 01:05:43 -0300

    Refatora metodos de manipulaçao do cliente para retornar valores puros

--- Funcional/Cliente.hs
+++ Funcional/Cliente.hs
@@ -1,17 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Cliente (
-    addCliente,
-    deletaCliente,
-    recuperaClientes,
-    recuperaClienteViaCpf,
-    recuperaNomeCliente,
-    formataExibicaoCliente,
-    toString
-) where
+module Cliente  where
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 import Database.SQLite.Simple.ToField
 import Database.SQLite.Simple.Types
+
+import Util (queryBD, fromIO, executeBD)
+
 
 -- definição do tipo Cliente, este será o objeto a ser armazenado no banco de dados.
 data Cliente = Cliente{
@@ -35,64 +30,61 @@ instance ToRow Cliente where
 
 -- método responsavel por adicionar o objeto cliente no banco de dados 
 --a partir do nome, cpf, telefone e endereco.
-addCliente :: String -> String -> String -> String -> IO ()
+addCliente :: String -> String -> String -> String -> IO [Cliente]
 addCliente nome cpf telefone endereco = do
-    conn <- open "../dados/aloka.db"
-    execute_ conn "CREATE TABLE IF NOT EXISTS clientes(\
+    criaBD
+    insereDado nome cpf telefone endereco
+    return (recuperaClienteViaCpf cpf)
+
+insereDado :: String -> String -> String -> String -> IO()
+insereDado nome cpf telefone endereco = do
+    executeBD ("INSERT INTO clientes(nome,\
+                \ cpf, \
+                \ telefone,\
+                \ endereco)\
+                \ VALUES\
+                \ ('" ++ nome ++ "',\
+                \ '" ++ cpf ++ "',\
+                \ '" ++ telefone ++ "',\
+                \ '" ++ endereco ++ "');") ()
+
+criaBD :: IO ()
+criaBD = do executeBD "CREATE TABLE IF NOT EXISTS clientes(\
                    \nome TEXT,\
                    \cpf TEXT PRIMARY KEY,\
                    \telefone TEXT,\
                    \endereco INTEGER\
-                   \);"
+                   \);" ()
 
-    execute conn "INSERT INTO clientes(nome,\
-                  \ cpf,\
-                  \telefone,\
-                  \endereco)\
-                  \ VALUES (?, ?, ?, ?)"
-                  (Cliente nome cpf telefone endereco)
 
 -- metodo responsavel por deletar o cliente que possui o cpf passado como parametro 
 deletaCliente :: String -> IO()
-deletaCliente cpf = do
-    conn <- open "../dados/aloka.db"
-    execute conn "DELETE FROM clientes WHERE cpf = ?"(Only cpf)
-
+deletaCliente cpf = executeBD ("DELETE FROM clientes WHERE cpf = '" ++ cpf ++ "';") ()
 
 -- Metodo responsavel por retornar uma lista que contém todos os clientes cadastrados no banco de dados.
-recuperaClientes :: IO [Cliente]
-recuperaClientes = do
-    conn <- open "../dados/aloka.db"
-    query_ conn "SELECT * FROM clientes"
+recuperaClientes :: [Cliente]
+recuperaClientes = fromIO (queryBD "SELECT * FROM clientes")
 
 -- Metodo responsavel por retornar uma lista que contém o cliente com o cpf passado como parametro
-recuperaClienteViaCpf :: String -> IO [Cliente]
-recuperaClienteViaCpf cpf = do
-    conn <- open "../dados/aloka.db"
-    query conn "SELECT * FROM clientes WHERE cpf = ?"(Only cpf) :: IO [Cliente]
+recuperaClienteViaCpf :: String -> [Cliente]
+recuperaClienteViaCpf cpf = fromIO (queryBD ("SELECT * FROM clientes WHERE cpf = '" ++ cpf ++ "';"))
 
 -- Metodo responsavel por retornar uma string que contém o nome do cliente que possui o cpf passado como parametro.
-recuperaNomeCliente :: String -> IO String
-recuperaNomeCliente cpf = do
-    clientes <- recuperaClienteViaCpf cpf
-    return (nome(head clientes))
+recuperaNomeCliente :: String -> String
+recuperaNomeCliente cpf = nome (head (recuperaClienteViaCpf cpf))
 
 -- Metodo responsavel por verificar se o cliente está cadastrado no banco de dados
 -- se o cliente estiver, retornará True, caso contrário False.
-verificaExistenciaCliente :: String -> IO Bool
-verificaExistenciaCliente cpf = do
-    clientes <- recuperaClienteViaCpf cpf
-    if null clientes then
-        return False
-    else
-        return True
+verificaExistenciaCliente :: String -> Bool
+verificaExistenciaCliente cpf
+    | null (recuperaClienteViaCpf cpf) = False 
+    | otherwise = True
 
 -- metodo responsavel por formatar a lista de clientes em uma string mais organizada.
 formataExibicaoCliente :: [Cliente] -> [String]
-formataExibicaoCliente [] = []
-formataExibicaoCliente (cliente:clientes) =  toString cliente : formataExibicaoCliente clientes
+formataExibicaoCliente = map toString
 
 -- metodo responsavel pela representação dos atributos do Cliente de forma textual.
 toString :: Cliente -> String
-toString cliente = "Nome: " ++ nome cliente  ++ ", cpf: " ++ cpf cliente ++ ", telefone: " 
+toString cliente = "Nome: " ++ nome cliente  ++ ", cpf: " ++ cpf cliente ++ ", telefone: "
     ++ telefone cliente ++ ", endereco: "  ++ endereco cliente

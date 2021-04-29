5b39ddae14e4f611171c855198ed3f982c5406e7
claudiodantas <franciclaudio.silva@ccc.ufcg.edu.br>
2021-04-28 21:12:07 -0300

    refatora cadastraFilme

--- Funcional/Filme.hs
+++ Funcional/Filme.hs
@@ -4,9 +4,7 @@ import FilmeDB
 
 
 cadastraFilme :: String -> String -> String -> String -> Int -> String
-cadastraFilme titulo diretor dataLancamento genero quantidade = do
-    if FilmeDB.verificaExistenciaFilmePorTitulo titulo
-        then "Erro: filme já cadastrado!"
-    else do {
-        "Filme - " ++ FilmeDB.cadastraFilme titulo genero diretor dataLancamento quantidade ++ " cadastrado com sucesso!"
-    }
+cadastraFilme titulo diretor dataLancamento genero quantidade
+    | FilmeDB.verificaExistenciaFilmePorTitulo titulo = "Erro: filme já cadastrado!"
+    | otherwise = "Filme - " ++ titulo ++ " - com id " ++ show idFilme ++ " cadastrado com sucesso!"
+    where idFilme = id_filme (FilmeDB.cadastraFilme titulo genero diretor dataLancamento quantidade)
